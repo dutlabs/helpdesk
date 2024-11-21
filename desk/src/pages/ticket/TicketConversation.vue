1a709116b3bd94f9a469f067e24928a8dadcd219
flaviacastro <fcastrb@gmail.com>
2024-11-21 11:55:00 -0300

    fix: reflect agent comments in user page

--- desk/src/pages/ticket/TicketConversation.vue
+++ desk/src/pages/ticket/TicketConversation.vue
@@ -6,7 +6,7 @@
   </div>
   <div class="grow overflow-auto px-5 pb-20">
     <div
-      v-for="c in communications"
+      v-for="c in allCommunications"
       :id="c.name"
       :key="c.name"
       class="items-between relative mt-4 flex justify-center gap-4"
@@ -57,9 +57,13 @@ const props = withDefaults(defineProps<P>(), {
 });
 const route = useRoute();
 const ticket = inject(ITicket);
-const communications = computed(() => {
-  const _communications = ticket.data.communications || [];
-  return orderBy(_communications, (c) => dayjs(c.creation));
+const allCommunications = computed(() => {
+  const combined = [
+    ...(ticket.data.communications || []),
+    ...(ticket.data.comments || []),
+  ];
+
+  return orderBy(combined, (c) => dayjs(c.creation));
 });
 
 function scroll(id: string) {
