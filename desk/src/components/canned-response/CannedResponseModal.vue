53ad5b07243540285ae6bc9e271eaab201bb2187
flaviacastro <fcastrb@gmail.com>
2025-02-06 16:24:11 -0300

    feat: add translation function to canned response components

--- desk/src/components/canned-response/CannedResponseModal.vue
+++ desk/src/components/canned-response/CannedResponseModal.vue
@@ -5,7 +5,7 @@
       size: 'xl',
       actions: [
         {
-          label: !isNew ? 'Update' : 'Create',
+          label: !isNew ? __('Update') : __('Create'),
           variant: 'solid',
           onClick: () => updateItem(),
         },
@@ -15,29 +15,33 @@
     <template #body-title>
       <div class="flex items-center gap-3">
         <h3 class="text-2xl font-semibold leading-6 text-gray-900">
-          {{ !isNew ? "Edit Canned Response" : "Create Canned Response" }}
+          {{
+            !isNew ? __("Edit Canned Response") : __("Create Canned Response")
+          }}
         </h3>
       </div>
     </template>
     <template #body-content>
       <div class="flex flex-col gap-4">
         <div>
-          <div class="mb-1.5 text-sm text-gray-600">{{ "Title" }}</div>
+          <div class="mb-1.5 text-sm text-gray-600">{{ __("Title") }}</div>
           <TextInput
             v-model="title"
             variant="outline"
-            :placeholder="'Customer Query Resolved'"
+            :placeholder="__('Customer Query Resolved')"
           />
         </div>
         <div>
-          <div class="mb-1.5 text-sm text-gray-600">{{ "Message" }}</div>
+          <div class="mb-1.5 text-sm text-gray-600">{{ __("Message") }}</div>
           <TextEditor
             ref="content"
             variant="outline"
             editor-class="!prose-sm overflow-auto min-h-[180px] max-h-80 py-1.5 px-2 rounded border border-gray-300 bg-white hover:border-gray-400 hover:shadow-sm focus:bg-white focus:border-gray-500 focus:shadow-sm focus:ring-0 focus-visible:ring-2 focus-visible:ring-gray-400 text-gray-800 transition-colors"
             :bubble-menu="true"
             :content="message"
-            :placeholder="'Your query has been resolved. Thank you for reaching out.'"
+            :placeholder="
+              __('Your query has been resolved. Thank you for reaching out.')
+            "
             @change="(val) => (message = val)"
           />
         </div>
